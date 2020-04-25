253045f0edf4c5a8d2a95def47a0e385cf4f6805
Thomas Weber <twebe@outlook.de>
2020-04-25 18:56:00 +0200

    Implemented AWS Lambda function with Express web framework handler

--- course-04/exercises/lesson-6/client/src/config.ts
+++ course-04/exercises/lesson-6/client/src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://1o91kbwvq3.execute-api.us-east-1.amazonaws.com/dev'
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'dev-r7gvvr6e.eu.auth0.com',
+  clientId: 'YfxAr0lMjwZBsXbWq4p80cOEnoluJROG',
   callbackUrl: 'http://localhost:3000/callback'
 }
